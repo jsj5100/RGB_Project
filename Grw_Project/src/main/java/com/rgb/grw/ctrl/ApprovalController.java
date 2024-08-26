4a4bc6f6f296ec95ed9e9c33bf6914da0a1ed573
Goodgaring <hori125@naver.com>
2024-08-26 10:00:47 +0900

    ApprovalController 생성

---
+++ Grw_Project/src/main/java/com/rgb/grw/ctrl/ApprovalController.java
@@ -0,0 +1,16 @@
+package com.rgb.grw.ctrl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class ApprovalController {
+	
+	@GetMapping(value = "/writeDocument.do")
+	public String writeDocument() {
+		return "writeDocument";
+	}
+}
