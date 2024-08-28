f36119f12e8b2065cc13ce4467578cd019eca5b6
jsj5100 <jsjjsj9710@gmail.com>
2024-08-28 14:03:12 +0900

    2024-08-28 로그인 관련 dto 및 mapper 수정

--- Grw_Project/src/main/java/com/rgb/grw/dto/UserInfoDto.java
+++ Grw_Project/src/main/java/com/rgb/grw/dto/UserInfoDto.java
@@ -17,9 +17,9 @@ import lombok.ToString;
 @NoArgsConstructor
 public class UserInfoDto {
 
-    private String emp_no;        
-    private String emp_password;  
-    private String emp_phone;     
+	private String emp_no;
+	private String emp_password;
+	private String emp_phone;
 	private String emp_name;
 	private String emp_email;
 	private String emp_gender;
@@ -30,8 +30,9 @@ public class UserInfoDto {
 	private String emp_state;
 	private Integer emp_fulloff;
 	private Integer emp_restoff;
-	private Integer emp_useoff; 
-	
-	
-}
+	private Integer emp_useoff;
+	private Integer dep_no;
+	private Integer auth_no;
+	private Integer tier_no;
 
+}
